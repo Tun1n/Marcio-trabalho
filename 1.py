5feb16f16412256ed13526d2d77a4501d233a9ec
Tun1n <slpedro21@outlook.com>
2023-09-11 20:02:53 -0300

    Exercícios gallo

---
+++ 1.py
@@ -0,0 +1,5 @@
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+soma = numero1 + numero2
+print(f"A soma entre {numero1} + {numero2} é {soma}.")
---
+++ 2.py
@@ -0,0 +1,3 @@
+valor1 = float(input("Digite o primeiro valor: "))
+valor2 = float(input("Digite o segundo valor: "))
+produto = valor1 * valor2
