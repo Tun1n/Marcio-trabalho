0544d16b52bac2c3044fce41dc2c55a1cc566235
Antônio Pedro <113259985+Tun1n@users.noreply.github.com>
2023-11-02 11:13:24 -0300

    Delete Python 2 trabalho directory

--- Python 2 trabalho/5.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# Carregar o dataset
-data = pd.read_csv('billions.csv')
-
-# 1. Liste as top 5 categorias com o maior número de bilionários.
-top_5_categorias = data['category'].value_counts().head(5)
-print("Top 5 categorias com o maior número de bilionários:")
-print(top_5_categorias)
